a1578a5dbbb468d848ea6aaf49c8a43fc67fadee
Pete Heyes <peteheyes@Leighs-MBP-4.lan>
2020-05-11 23:12:08 +0100

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios'
 
-function App() {
+export const App = () => {
+  const [ address1, setAddress1 ] = useState('')
+  const [ address2, setAddress2 ] = useState('')
+  const [ radius, setRadius ] = useState(200)
+  const [ coordinates, setCoordinates ] = useState({})
+  const [ loading, setLoading ] = useState(false)
+
+  const getCoordinates = async () => {
+    setLoading(true)
+    try {
+      const apiResponse = await axios.get('https://maps.googleapis.com/maps/api/directions/json?origin=”49 Colvestone Cres, Clapton, London E8 2LG, UK”&destination=”Station Passage, Peckham, London SE15 2JR, UK”&key=PUT API KEY HERE')
+      setCoordinates({data: apiResponse.data})
+      setLoading(false)
+    } 
+    catch(error) {
+      console.log(error)
+    } 
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        value={address1}
+        onChange={e => setAddress1(e.target.value)}
+        placeholder="Address 1"
+      ></input>
+      <input
+        type="text"
+        value={radius}
+        onChange={e => setRadius(e.target.value)}
+        placeholder="Search Radius (m)"
+      ></input>
+      <input
+        type="text"
+        value={address2}
+        onChange={e => setAddress2(e.target.value)}
+        placeholder="Address 2"
+      ></input>
+      <button
+      onClick={() => getCoordinates()}
+      >GO</button>
     </div>
   );
 }
-
-export default App;
